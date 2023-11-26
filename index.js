99b4a521c2cc462b9b72e5de343862d115144dff
hoseintyi <hoseint629@gmail.com>
2023-11-26 11:43:17 +0330

    add get request

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/" , (req , res)=>{
+    res.send("<h1>hello world</h1>");
+});
+
 app.listen(3000 , ()=>{
     console.log("server now running on port 3000!");
 });
