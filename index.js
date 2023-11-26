63c6104a0bb2848ad81af6c651ab5a286ebf84e2
hoseintyi <hoseint629@gmail.com>
2023-11-26 13:15:13 +0330

    first post http request

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get("/" , (req , res)=>{
     res.send("<h1>hello world</h1>");
 });
 
+app.post("/Register" , (req , res)=>{
+    res.sendStatus(200);
+});
+
+
 app.listen(3000 , ()=>{
     console.log("server now running on port 3000!");
 });
