bdd2427626ba0494926a1646937365fa1866b61f
hoseintyi <hoseint629@gmail.com>
2023-11-28 00:01:16 +0330

    add new custom middleware - commit with node-modules

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get("/" , (req , res)=>{
+function logger(req, res, next) {
+    console.log("your request is " + req.method);
+    console.log("your URL is " + req.url);
+    next();
+}
+
+app.use(logger);
+
+app.get("/", (req, res) => {
     res.send("<h1>hello world</h1>");
 });
 
-app.post("/Register" , (req , res)=>{
+app.post("/Register", (req, res) => {
     res.sendStatus(200);
 });
 
 
-app.listen(3000 , ()=>{
+app.listen(3000, () => {
     console.log("server now running on port 3000!");
 });
